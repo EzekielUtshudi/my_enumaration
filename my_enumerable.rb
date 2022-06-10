fbd4480ec8b2f6978e8d7ff20ac1a5a2b5a43179
maxthestranger <thestranger@gmail.com>
2022-06-10 11:50:42 +0300

    fix linters

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,22 +1,22 @@
 # Module defined in my_enumerable.rb file
 module MyEnumerable
-    def all?
-      return true unless block_given?
-  
-      each { |n| return false unless yield(n) }
-      true
-    end
-  
-    def any?
-      return true unless block_given?
-  
-      each { |n| return true if yield(n) }
-      false
-    end
-  
-    def filter?
-      arr = []
-      each { |n| arr.push(n) if yield(n) }
-      arr
-    end
+  def all?
+    return true unless block_given?
+
+    each { |n| return false unless yield(n) }
+    true
   end
+
+  def any?
+    return true unless block_given?
+
+    each { |n| return true if yield(n) }
+    false
+  end
+
+  def filter?
+    arr = []
+    each { |n| arr.push(n) if yield(n) }
+    arr
+  end
+end
